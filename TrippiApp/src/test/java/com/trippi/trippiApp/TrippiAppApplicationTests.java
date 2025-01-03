5f0f92a42860e7600100fea1e589c8c3b0cb9e7e
Robert Wyszynski <robertointerwento@gmail.com>
2025-01-03 01:15:12 +0100

    add registration form

---
+++ TrippiApp/src/test/java/com/trippi/trippiApp/TrippiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trippi.trippiApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrippiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
